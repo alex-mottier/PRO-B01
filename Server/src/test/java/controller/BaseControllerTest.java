3dd4b56c64d7e48d628d4caef680e2dc755aa5cf
hakimbalestrieri <hakim.balestrieri@travizory.com>
2021-03-22 18:31:28 +0100

    initialization of baseControllerTest

--- Server/src/test/java/controller/BaseControllerTest.java
+++ Server/src/test/java/controller/BaseControllerTest.java
@@ -1,13 +1,22 @@
-package com.example.controller;
+package controller;
 
+import com.example.controller.BaseController;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseControllerTest {
+
+    BaseController baseController = new BaseController();
+
     @Test
     void shouldReturnHelloOrValue() {
-        BaseController baseController = new BaseController();
         assertEquals(baseController.testBaseMethod("Hakim"), "Hakim");
+    }
+
+    @Test
+    void shouldBeBaseControllerClass() {
+        assertEquals(baseController.controllerName(), "BaseController");
     }
 }
