4b913cdaa69cdd01b4ab048172b0d431f806120d
Alexis-Allemann <alexis.allemann@gmail.com>
2021-03-22 14:20:31 +0100

    Création fichier configurations globales

---
+++ Client/src/app/context/Globals.ts
@@ -0,0 +1,24 @@
+/**-----------------------------------------------------------------------------------
+ * @file    Global.ts
+ * @author  Alexis Allemann
+ * @date    22.03.2021
+ * @brief   Paramètres généraux de l'application
+ -----------------------------------------------------------------------------------*/
+
+export default {
+  COLORS: {
+    PRIMARY: '#eb3b28',
+    BLUE: '#28d7eb',
+    PINK: '#eb2876',
+    ORANGE: '#eb9d28',
+    YELLOW: '#d7eb28',
+    GREEN: '#28eb3c',
+  },
+  ICONS: {
+    ARROW_LEFT: 'arrow-left',
+  },
+  PRECISION: 2,
+  SIZES: {
+    ICON_BUTTON: 30,
+  },
+};
