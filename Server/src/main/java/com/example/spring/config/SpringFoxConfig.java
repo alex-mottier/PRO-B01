19fdc1c2990ed6ac2946b8ccd9a92a1a080dd0f6
hakimbalestrieri <hakim.balestrieri@travizory.com>
2021-03-24 16:36:14 +0100

    base project configuration, can run mongodb and neo4j locally now

--- Server/src/main/java/com/example/spring/config/SpringFoxConfig.java
+++ Server/src/main/java/com/example/spring/config/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.example.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,12 +16,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class SpringFoxConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiEndpointsInfo())
+                .tags(
+                        new Tag("Amphytrion", "PRO - Application project")
+                );
+    }
+    private ApiInfo apiEndpointsInfo() {
+        return new ApiInfoBuilder().title("Amphytrion API documentation")
+                .description("take a look at README.md")
+                .version("1.0.0")
                 .build();
     }
 }
