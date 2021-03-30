6aead5440b516cc8d248e6cfa6dee01297832c2a
Alexis-Allemann <alexis.allemann@gmail.com>
2021-03-30 15:57:49 +0200

    Authentification avec Google : OK

--- Client/src/app/models/User.ts
+++ Client/src/app/models/User.ts
@@ -5,15 +5,20 @@
  * @brief   User of the application
  */
 
+import { TokenResponse } from 'expo-app-auth';
+
 export default class User {
   private name: string;
+  private token: TokenResponse;
 
   /**
    * Instantiation of a new user
    * @param name of the user
+   * @param token of the user
    */
-  public constructor(name: string) {
+  public constructor(name: string, token: TokenResponse) {
     this.name = name;
+    this.token = token;
   }
 
   /**
@@ -22,5 +27,13 @@ export default class User {
    */
   public getName(): string {
     return this.name;
+  }
+
+  /**
+   * Get the user token
+   * @returns the user token
+   */
+  public getToken(): TokenResponse {
+    return this.token;
   }
 }
