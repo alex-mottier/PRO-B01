76dfe0ced203d534d0e8621793446f1a84e48ba4
AloisChristen <alois.christen@heig-vd.ch>
2021-04-09 22:03:28 +0200

    TokenValidator, must be tested

--- Server/src/main/java/ch/amphytrion/project/controller/UserController.java
+++ Server/src/main/java/ch/amphytrion/project/controller/UserController.java
@@ -2,14 +2,25 @@ package ch.amphytrion.project.controller;
 
 import ch.amphytrion.project.entities.User;
 import ch.amphytrion.project.services.UserService;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -20,6 +31,30 @@ public class UserController extends BaseController implements IGenericController
     @Autowired
     UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/connect")
+    public ResponseEntity<Boolean> connect(@RequestBody String idTokenString) {
+        String ClIENT_ID = "298748587556-mpio0261lovc0qkt660nbhgariolp1no.apps.googleusercontent.com";
+        JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JSON_FACTORY)
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList(ClIENT_ID))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+        try {
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            if (idToken != null) {
+                Payload payload = idToken.getPayload();
+                return ResponseEntity.ok(null);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } catch (IOException | GeneralSecurityException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/users")
