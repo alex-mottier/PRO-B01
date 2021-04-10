4fa86233556d05c244866d3f995141196a9cc97d
kainomad <johannwerklegit@gmail.com>
2021-04-10 17:03:56 +0200

    update of repositories for the id

--- Server/src/main/java/ch/amphytrion/project/repositories/HostRepository.java
+++ Server/src/main/java/ch/amphytrion/project/repositories/HostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HostRepository extends MongoRepository<Host, Long> {
+public interface HostRepository extends MongoRepository<Host, String> {
 }
--- Server/src/main/java/ch/amphytrion/project/repositories/UserRepository.java
+++ Server/src/main/java/ch/amphytrion/project/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package ch.amphytrion.project.repositories;
 
 import ch.amphytrion.project.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends Neo4jRepository<User, String> {
 
     User findByUsername(String username);
 
