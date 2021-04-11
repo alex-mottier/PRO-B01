7c4900b7f13d1bd0fb136e05c1640e92a78cd02f
Alexandre Mottier <alex_mottier@hotmail.com>
2021-04-11 11:39:28 +0200

    Création du composant "Location"

---
+++ Client/src/components/Location/LocationComponent.tsx
@@ -0,0 +1,77 @@
+/**
+ * @file    Meeting.tsx
+ * @author  Alexis Allemann & Alexandre Mottier
+ * @date    08.04.2021
+ * @brief   Meeting component
+ */
+
+import * as React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+import { Card, Avatar, Text, Chip, IconButton } from 'react-native-paper';
+import Globals from '../../app/context/Globals';
+import { Location, Tag } from '../../app/models/ApplicationTypes';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+interface IProps {
+  location: Location;
+  onClose(): void | null;
+}
+
+const LocationComponent: React.FC<IProps> = ({ location }) => {
+  let nbColors = 0;
+  const colors = [
+    Globals.COLORS.ORANGE,
+    Globals.COLORS.GREEN,
+    Globals.COLORS.GREEN,
+    Globals.COLORS.YELLOW,
+    Globals.COLORS.BLUE,
+    Globals.COLORS.BLUE,
+    Globals.COLORS.ORANGE,
+    Globals.COLORS.GREEN,
+  ];
+  return (
+    <Card style={styles.card}>
+      <Card.Title
+        title={location.name}
+        subtitle={location.desciption}
+        left={() => <Avatar.Image size={40} source={require('../../../assets/HEIG-VD.png')} />}
+        right={() => (
+          <View>
+            <View style={styles.nbPeople}>
+              <Text>{location.nbPeople}</Text>
+              <MaterialCommunityIcons
+                name={Globals.ICONS.PROFILE}
+                color={Globals.COLORS.GRAY}
+                size={Globals.SIZES.ICON_BUTTON}
+              />
+            </View>
+          </View>
+        )}
+      />
+      <Card.Content>
+        <View style={styles.chips}>
+          {location.tags.map((tag: Tag) => {
+            return (
+              <Chip
+                key={tag.name}
+                style={[styles.chip, { backgroundColor: colors[nbColors++ % colors.length] }]}>
+                {tag.name}
+              </Chip>
+            );
+          })}
+        </View>
+        <View style={styles.close}>
+          <IconButton
+            icon={Globals.ICONS.CLOSE_LOCATION}
+            size={Globals.SIZES.ICON_BUTTON}
+            color={Globals.COLORS.GRAY}
+            style={styles.icon}
+          />
+        </View>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default LocationComponent;
