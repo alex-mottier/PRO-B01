dcbb0ce29aa3b5b9e0ac7dbf11e3c82450e36b2f
AloisChristen <alois.christen@heig-vd.ch>
2021-04-16 20:37:51 +0200

    ByeBye neo4j

--- Server/src/main/java/ch/amphytrion/project/repositories/UserRepository.java
+++ Server/src/main/java/ch/amphytrion/project/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package ch.amphytrion.project.repositories;
 
 import ch.amphytrion.project.entities.User;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends Neo4jRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
 
     User findByUsername(String username);
 
