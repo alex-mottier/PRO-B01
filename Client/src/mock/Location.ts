e580c0c61a8fc9eb8e2569c8ef0da2cf82f6086d
Alexandre Mottier <alex_mottier@hotmail.com>
2021-04-17 00:12:56 +0200

    Création du mock pour une seule location

---
+++ Client/src/mock/Location.ts
@@ -0,0 +1,35 @@
+import { Day, DayEnum, Location } from '../app/models/ApplicationTypes';
+
+let date = new Date();
+let endDate = new Date(date.getHours() + 1);
+
+export const mockLocation: Location = {
+  name: 'G01',
+  description: 'Salle de cours avec Wifi',
+  tags: [{ name: 'Salle de cours' }, { name: 'HEIG-VD' }],
+  nbPeople: 10,
+  openingHours: [
+    {
+      id: '1',
+      startTime: date,
+      endTime: endDate,
+      days: [
+        { name: 'Lundi', day: DayEnum.MON },
+        { name: 'Vendredi', day: DayEnum.FRI },
+      ],
+    },
+    {
+      id: '2',
+      startTime: date,
+      endTime: endDate,
+      days: [{ name: 'Samedi', day: DayEnum.SAT }],
+    },
+    {
+      id: '3',
+      startTime: date,
+      endTime: endDate,
+      days: [{ name: 'Dimanche', day: DayEnum.SUN }],
+    },
+  ],
+  host: { name: 'HEIG-VD' },
+};
