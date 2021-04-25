68cfddf0717eadcea5f3434ce05e2a49cbc8bbef
hakimbalestrieri <hakim.balestrieri@travizory.com>
2021-04-25 22:01:54 +0200

    ajout id dans dto user

--- Server/src/main/java/ch/amphytrion/project/dto/UserResponse.java
+++ Server/src/main/java/ch/amphytrion/project/dto/UserResponse.java
@@ -4,8 +4,9 @@ import ch.amphytrion.project.entities.databaseentities.User;
 
 public class UserResponse implements InterfaceDTO {
     public String username;
-
+    public String id;
     public UserResponse(User user) {
         this.username = user.getUsername();
+        this.id = user.getId();
     }
 }
