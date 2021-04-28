1faa90b642d659250654f3b9af83a3c453a6fdd3
alex-mottier <alexandre.mottier@heig-vd.ch>
2021-04-28 21:19:21 +0200

    Création des screens

--- Client/src/screens/Host/Profile/Profile.tsx
+++ Client/src/screens/Host/Profile/Profile.tsx
@@ -9,23 +9,17 @@ import * as React from 'react';
 import { SafeAreaView, ScrollView, View } from 'react-native';
 import {
     Provider,
-    useTheme,
 } from 'react-native-paper';
 
-import styles from './styles';
 import { observer } from 'mobx-react-lite';
-// import GlobalStore from '../../../app/stores/GlobalStore';
+
 
 const Profile: React.FC = () => {
-    // Usage of react native paper theme library
-    const paperTheme = useTheme();
 
-    /* Usage of MobX global state store */
-    //const store = React.useContext(GlobalStore);
     return (        <Provider>
         <SafeAreaView>
             <ScrollView>
-                <View style={[styles.container, { backgroundColor: paperTheme.colors.surface }]}>
+                <View>
 
                 </View>
             </ScrollView>
