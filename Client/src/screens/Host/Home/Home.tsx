e7264ea4bdb89b496cef1820ee0da77eb2bff1f8
alex-mottier <alexandre.mottier@heig-vd.ch>
2021-04-28 21:19:21 +0200

    Création des screens

--- Client/src/screens/Host/Home/Home.tsx
+++ Client/src/screens/Host/Home/Home.tsx
@@ -1,41 +1,87 @@
 /**
- * @file    Search.tsx
+ * @file    Home.tsx
  * @author  Alexis Allemann & Alexandre Mottier
  * @date    04.03.2021
- * @brief   Search meeting page
+ * @brief   Student home page
  */
 
 import * as React from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
-import {
-    Provider,
-    useTheme,
-} from 'react-native-paper';
-
+import { View } from 'react-native';
+import { IconButton, useTheme } from 'react-native-paper';
 import styles from './styles';
+import { Agenda } from 'react-native-calendars';
+import Globals from '../../../app/context/Globals';
+import { LocaleConfig } from 'react-native-calendars';
 import { observer } from 'mobx-react-lite';
-// import GlobalStore from '../../../app/stores/GlobalStore';
+import MeetingComponent from '../../../components/Meeting/MeetingComponent';
+import { Meeting } from '../../../app/models/ApplicationTypes';
+import { addYears, format } from 'date-fns';
+import { dateLocale } from '../../../app/context/DateFormat';
+import { useStores } from '../../../app/context/storesContext';
 
-const Search: React.FC = () => {
-    // Usage of react native paper theme library
-    const paperTheme = useTheme();
+// Format date definition
+LocaleConfig.locales['fr'] = dateLocale;
+LocaleConfig.defaultLocale = 'fr';
 
+const Home: React.FC = () => {
     /* Usage of MobX global state store */
-    //const store = React.useContext(GlobalStore);
-
-
-
+    const { hostStore } = useStores();
+    const paperTheme = useTheme();
     return (
-        <Provider>
-            <SafeAreaView>
-                <ScrollView>
-                    <View style={[styles.container, { backgroundColor: paperTheme.colors.surface }]}>
-
+        <Agenda
+            items={hostStore.items ? hostStore.items : {}}
+            pastScrollRange={1}
+            futureScrollRange={12}
+            selected={format(new Date(), 'yyyy-MM-dd')}
+            minDate={format(new Date(), 'yyyy-MM-dd')}
+            maxDate={format(addYears(new Date(), 1), 'yyyy-MM-dd')}
+            onDayPress={(day) => {
+                const newDate = new Date(day.dateString);
+                hostStore.generateItems(newDate);
+            }}
+            renderItem={(item) => {
+                return (
+                    <View style={styles.meetings}>
+                        <View style={styles.meeting}>
+                            <MeetingComponent
+                                meeting={item}
+                                isSearchView={false}
+                                isChatable={true}
+                                isInCalendar={true}
+                            />
+                        </View>
                     </View>
-                </ScrollView>
-            </SafeAreaView>
-        </Provider>
+                );
+            }}
+            renderEmptyDate={() => {
+                return <View></View>;
+            }}
+            renderKnob={() => {
+                return (
+                    <View style={styles.knob}>
+                        <IconButton
+                            icon={Globals.ICONS.ARROW_DOWN}
+                            size={Globals.SIZES.ICON_MENU}
+                            color={Globals.COLORS.GRAY}
+                        />
+                    </View>
+                );
+            }}
+            rowHasChanged={(r1: Meeting, r2: Meeting) => {
+                return r1 !== r2;
+            }}
+            theme={{
+                calendarBackground: paperTheme.colors.surface,
+                backgroundColor: paperTheme.colors.surface,
+                agendaDayTextColor: Globals.COLORS.TEXT,
+                agendaDayNumColor: Globals.COLORS.TEXT,
+                agendaTodayColor: Globals.COLORS.PRIMARY,
+                agendaKnobColor: Globals.COLORS.PRIMARY,
+                dotColor: Globals.COLORS.BLUE,
+                selectedDayBackgroundColor: Globals.COLORS.BLUE,
+            }}
+        />
     );
 };
 
-export default observer(Search);
+export default observer(Home);
--- Client/src/screens/Host/Home/styles.ts
+++ Client/src/screens/Host/Home/styles.ts
@@ -2,84 +2,28 @@
  * @file    styles.ts
  * @author  Alexis Allemann & Alexandre Mottier
  * @date    04.03.2021
- * @brief   Search meeting page styles
+ * @brief   Student home page styles
  */
 
 import { StyleSheet } from 'react-native';
-import Globals from '../../../app/context/Globals';
 
 const styles = StyleSheet.create({
-  button: {
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  close: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-  },
-  content: {
-    width: '100%',
-    alignItems: 'center',
-  },
-  modalcontainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 20,
-  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 20,
+    margin: 15,
   },
-  fab: {
-    marginTop: 10,
-    marginLeft: 10,
-  },
-  fields: {
-    width: '80%',
-  },
-  date: {
-    paddingLeft: 10,
+  meetings: {
+    display: 'flex',
     flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  dateHeure: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginLeft: 10,
-    marginTop: 10,
-  },
-  modal: {
-    padding: 10,
-    width: '100%',
-    alignItems: 'center',
-  },
-  search: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    marginBottom: 20,
-    alignItems: 'center',
-  },
-  title: {
     justifyContent: 'center',
-    textAlign: 'center',
-    marginBottom: 20,
-    color: Globals.COLORS.BLUE,
   },
-  main: {
-    width: '100%',
-    padding: 10,
+  meeting: {
+    width: '90%',
+  },
+  knob: {
+    marginTop: -15,
   },
 });
 
