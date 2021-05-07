31a86b3c618b8f07df30e1109a68058fe461a139
hakimbalestrieri <hakim.balestrieri@travizory.com>
2021-05-07 14:14:14 +0200

    edit LocalDateTime to LocalTime

--- Server/src/main/java/ch/amphytrion/project/entities/databaseentities/OpeningHour.java
+++ Server/src/main/java/ch/amphytrion/project/entities/databaseentities/OpeningHour.java
@@ -3,12 +3,14 @@ package ch.amphytrion.project.entities.databaseentities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.joda.time.format.DateTimeFormat;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Document
@@ -17,6 +19,7 @@ public class OpeningHour {
         SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY
     }
     private Integer day;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
 }
