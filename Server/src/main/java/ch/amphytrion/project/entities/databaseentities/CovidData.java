d4881e6d982bcd66e7d8e3914fbb8dda9206daa9
hakimbalestrieri <hakim.balestrieri@travizory.com>
2021-05-16 13:14:49 +0200

    resolving issue #68

--- Server/src/main/java/ch/amphytrion/project/entities/databaseentities/CovidData.java
+++ Server/src/main/java/ch/amphytrion/project/entities/databaseentities/CovidData.java
@@ -3,7 +3,6 @@ package ch.amphytrion.project.entities.databaseentities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
@@ -11,19 +10,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document
 public class CovidData {
-    @Id
-    private String id;
     private Boolean isOpen;
     private Boolean masksRequired;
     private Boolean disinfectionRequired;
     private String recommendedDistancing;
     private String comments;
-
-    public CovidData(Boolean isOpen, Boolean masksRequired, Boolean disinfectionRequired, String recommendedDistancing, String comments) {
-        this.isOpen = isOpen;
-        this.masksRequired = masksRequired;
-        this.disinfectionRequired = disinfectionRequired;
-        this.recommendedDistancing = recommendedDistancing;
-        this.comments = comments;
-    }
 }
